2c15c2464547daa6042766f24c22a6a337fcbe9f
driftingwood <driftingwood1@gmail.com>
2022-07-03 11:11:14 -0400

    rinkby test

--- backend/src/config.js
+++ backend/src/config.js
@@ -281,7 +281,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = [null]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "0x64dF73270BE92D3Fb33B2785cBbeb3f70f596245"; // If you want to manually include it
+let CONTRACT_ADDRESS = null; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = false; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
